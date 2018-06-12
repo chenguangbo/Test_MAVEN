2fb478e35527b8038a5f5414ed3a769d9c22f203
CGB <cgb2499404424@163.com>
2018-06-12 21:48:29 +0800

    删除多余
    2018年6月12日21:48:22  home

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,15 +2,5 @@ public class Test {
 
     public static void main(String[] arg){
         System.out.println("你好");
-        String s = "123";
-        char charAt = s.charAt(0);
-        System.out.println(charAt);
-
-
-
-
-
-
-        System.out.println("你好");   System.out.println("你好");
     }
 }
