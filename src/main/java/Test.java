1b7b9c8ba6254e70130a3fe87af6107b780238b0
CGB <cgb2499404424@163.com>
2018-06-12 22:07:06 +0800

    打乱
    2018年6月12日22:06:58  home

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,12 +1,20 @@
 public class Test {
 
     public static void main(String[] arg){
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         System.out.println("你好");
-        String s = "123";
-        char charAt = s.charAt(0);
-        System.out.println(charAt);
-
-
+       
 
 
     }
