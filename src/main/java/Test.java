13a5512a36e9f8860b27b0204a5cc49b3e8d149b
cgb2499404424@163.com <cgb2499404424@163.com>
2018-06-12 21:02:17 +0800

    添加打印

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,13 @@
 public class Test {
 
     public static void main(String[] arg){
-        
+        System.out.println("你好");
+
+
+
+
+
+
+        System.out.println("你好");   System.out.println("你好");
     }
 }
