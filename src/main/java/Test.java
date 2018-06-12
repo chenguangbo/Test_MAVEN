35dad1ee3db58551b7008b0483b03b109de76b46
cgb2499404424@163.com <cgb2499404424@163.com>
2018-06-12 22:08:37 +0800

    我们都是好孩子  2018年6月12日22:08:23  home

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,9 +2,9 @@ public class Test {
 
     public static void main(String[] arg){
         System.out.println("你好");
-        String s = "123";
-        char charAt = s.charAt(0);
-        System.out.println(charAt);
+
+
+        System.out.println("我们都是好孩子");
 
 
 
