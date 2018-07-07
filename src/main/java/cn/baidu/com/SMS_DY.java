c3fb51bfb975e9cdcc50246b0a74684a7f9f8bf1
cgb2499404424@163.com <cgb2499404424@163.com>
2018-07-07 09:40:55 +0800

    还要我充值   2018年7月7日09:40:05   home

--- src/main/java/cn/baidu/com/SMS_DY.java
+++ src/main/java/cn/baidu/com/SMS_DY.java
@@ -1,4 +1,42 @@
 package cn.baidu.com;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+
 public class SMS_DY {
+
+    public static void main(String[] args) throws Exception{
+        final String product = "Dysmsapi";
+        final String domain = "dysmsapi.aliyuncs.com";
+        final String accessKeyId = "";
+        final String accessKeySecret = "";
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
+                accessKeySecret);
+        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+        IAcsClient acsClient = new DefaultAcsClient(profile);
+        SendSmsRequest request = new SendSmsRequest();
+        request.setMethod(MethodType.POST);
+        //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式；发送国际/港澳台消息时，接收号码格式为00+国际区号+号码，如“0085200000000”
+        request.setPhoneNumbers("155");
+        //必填:短信签名-可在短信控制台中找到
+        request.setSignName("陈广波");
+        //必填:短信模板-可在短信控制台中找到
+        request.setTemplateCode("SMS_139450038");
+        request.setTemplateParam("{\"name\":\"Tom\", \"code\":\"123\"}");
+        //可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
+        //request.setSmsUpExtendCode("90997");
+        //请求失败这里会抛ClientException异常
+        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+        System.out.println(sendSmsResponse.getMessage() + "   " + sendSmsResponse.getRequestId() + "  " + sendSmsResponse.getCode());
+        if (sendSmsResponse.getCode() != null && sendSmsResponse.getCode().equals("OK")) {
+            System.out.println("SUCCESS");
+        }
+    }
+
+
 }
